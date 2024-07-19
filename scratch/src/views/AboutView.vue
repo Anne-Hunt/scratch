11baf5943f602834674e3723ff8bf2e566d307b9
Anne Hunt <acathunt@gmail.com>
2024-07-19 17:37:08 -0600

    updated about page

--- scratch/src/views/AboutView.vue
+++ scratch/src/views/AboutView.vue
@@ -1,6 +1,12 @@
+<script>
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About Scratch</h1>
+    <p>Scratch is a Vue.js, Bootstrap, Javascript, and Goblincore to the MAX website. It's created by Anne Hunt to illustrate her flexibility as a designer of web applications.</p>
+    <h2>What is Goblincore?</h2>
+    <p>It's an aesthetic defined by a love of pretty rocks, moss, trees, toads, and the outdoors. It's for all those who like a nice tea and a walk down a muddy path, who pick up pretty rocks and keep them in a jar. It's dark, imperfect, and cozy. Some of us like picking up animal bones on walks and cleaning them, then finding what type of animal they go to. Some of us like to watch the Oscars. Usually only one of those is goblincore.</p>
   </div>
 </template>
 
