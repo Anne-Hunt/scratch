7f1fa067d97eae91be50a09fb149a0816e818ac6
Anne Hunt <acathunt@gmail.com>
2024-07-19 17:49:03 -0600

    outlined three primary views

---
+++ scratch/src/components/thePlayground.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+</script>
+
+
+<template>
+<section class="row">
+    <section class="row sky bg-dark">
+
+    </section>
+    <section class="row ground bg-brown">
+
+    </section>
+</section>
+<section class="row bg-dark">
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+    <div class="col-3"></div>
+</section>
+<section class="row">
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+    <div class="col-1"></div>
+</section>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
